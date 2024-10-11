95155cd0e929384d7e12aafdb468653ce87cbe87
Piyush Khatri <piyushkhatri968@gmail.com>
2024-10-12 00:01:50 +0500

    two

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
+// require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const booksRoute = require("./routes/booksRoute.js");
 const app = express();
 
-// const PORT = process.env.PORT;
+const PORT = 8080;
 
 // Middleware for parsing request body
 app.use(express.json());
@@ -13,10 +13,12 @@ app.use(express.json());
 // Middleware for handling CORS POLICY
 app.use(cors());
 
-const mongo_URL = process.env.MONGO_URL;
+// const mongo_URL = process.env.MONGO_URL;
 
 mongoose
-  .connect(mongo_URL)
+  .connect(
+    "mongodb+srv://piyushkhatri968:BO9SicifrC9zwYmQ@cluster0.dwfhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
   .then(() => {
     console.log("App connected to database");
   })
