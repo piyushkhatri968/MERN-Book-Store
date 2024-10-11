70a4090814723bfc2906baeed64df0503071fd1b
Piyush Khatri <piyushkhatri968@gmail.com>
2024-10-12 00:49:46 +0500

    updated

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -4,7 +4,7 @@ const BooksCard = ({ books }) => {
   return (
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       {books &
-        books?.map((item) => <BookSingleCard key={item._id} book={item} />)}
+        books.map((item) => <BookSingleCard key={item._id} book={item} />)}
     </div>
   );
 };
