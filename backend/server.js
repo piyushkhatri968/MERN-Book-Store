827aa3b976509eb67aed123bd21b7c02e2f55a79
Piyush Khatri <piyushkhatri968@gmail.com>
2024-10-12 00:23:54 +0500

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const booksRoute = require("./routes/booksRoute.js");
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT;
 
 // Middleware for parsing request body
 app.use(express.json());
@@ -13,8 +13,7 @@ app.use(express.json());
 // Middleware for handling CORS POLICY
 app.use(cors());
 
-const mongo_URL =
-  "mongodb+srv://piyushkhatri968:BO9SicifrC9zwYmQ@cluster0.dwfhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const mongo_URL = process.env.MONGO_URL;
 
 mongoose
   .connect(mongo_URL)
@@ -32,6 +31,8 @@ app.get("/", (request, response) => {
 
 app.use("/books", booksRoute);
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`App is listening to port: ${PORT}`);
+// });
+
+module.exports = app;
